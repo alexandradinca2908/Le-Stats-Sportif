b24e9ada63d8e245d05f917c2dac997c6ce03e46
Alexandra Dinca <student@pcom.localdomain>
2025-03-30 16:55:17 +0300

    Began implementation for Thread Pool and Data Ingestor

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,9 @@ if not os.path.exists('results'):
     os.mkdir('results')
 
 webserver = Flask(__name__)
-webserver.tasks_runner = ThreadPool()
 
-# webserver.task_runner.start()
+webserver.tasks_runner = ThreadPool()
+webserver.tasks_runner.start()
 
 webserver.data_ingestor = DataIngestor("./nutrition_activity_obesity_usa_subset.csv")
 
--- app/task_runner.py
+++ app/task_runner.py
@@ -1,6 +1,9 @@
 from queue import Queue
-from threading import Thread, Event
+from threading import Thread, Event, Lock
 import time
+import os
+import multiprocessing
+import data_ingestor
 
 class ThreadPool:
     def __init__(self):
@@ -13,12 +16,56 @@ class ThreadPool:
         #   * create more threads than the hardware concurrency allows
         #   * recreate threads for each task
         # Note: the TP_NUM_OF_THREADS env var will be defined by the checker
+
+        #  Number of threads in threadpool
+        self.num_of_threads = 0
+        if os.environ['TP_NUM_OF_THREADS'] is not None:
+            self.num_of_threads = os.environ['TP_NUM_OF_THREADS'] 
+        else:
+            self.num_of_threads = multiprocessing.cpu_count()
+
+        #  Thread pool
+        self.threads = []
+        for i in range(self.num_of_threads):
+            self.threads.append(TaskRunner(i))
+        
+        #  Task queue
+        self.task_queue = []
+
+        #  Server status
+        self.server_is_on = True
+    
+        #  Events
+        self.data_ingestor_complete = Event()
+
+        #  Locks
+        self.task_queue_lock = Lock()
+        self.data_ingestor_lock = Lock()
+        self.results_lock = Lock()
+
+    #  Starts thread pool
+    def start(self):
+        for i in range(self.num_of_threads):
+            self.threads[i].start()
+    
+    #  Stops waits for threads to finish and closes server
+    def stop(self):
         pass
 
+    #  Notifies thread that they can start processing requests
+    def set_data_ingestor(self):
+        self.data_ingestor_complete.set()
+
+    #  Add task to queue
+    def submit_task(self, task):
+        with self.task_queue_lock:
+            self.task_queue.append(task)
+
 class TaskRunner(Thread):
-    def __init__(self):
+    def __init__(self, id):
         # TODO: init necessary data structures
-        pass
+        Thread.__init__(self)
+        self.id = id
 
     def run(self):
         while True:
